5b7b488d2b30f9f263e893c85f3d84d9a317dc03
dcdanko <dcdanko@gmail.com>
2016-06-04 13:41:49 +0100

    removed emailinfopage

--- emailinfopage/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-class AddInterestedEmailTests(TestCase):
-
-	def add_simple_email(self):
-		response = self.client.post('/postemail/', {'iemail':'test@email.com'})
